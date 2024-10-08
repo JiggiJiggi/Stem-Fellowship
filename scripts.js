51857924ab2913453fbbb8c096d8bec51bfae77f
Jignesh Bejjagam <69974777+JiggiJiggi@users.noreply.github.com>
2024-10-08 17:50:14 -0500

    Added remaining files

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// JavaScript for button click events
+function postJob() {
+    alert("Redirecting to Post a Job/Education...");
+    // Add the functionality to redirect to the post job page
+}
+
+function searchJob() {
+    alert("Redirecting to Search a Job/Education...");
+    // Add the functionality to redirect to the search job page
+}
